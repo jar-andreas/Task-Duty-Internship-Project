1c19240cb34415ded42ad2ec022ecebd2011af83
jar-andreas <adeyinkaayomide45@gmail.com>
2026-03-18 09:27:51 -0500

    Fix: Updated my UI for the Edit Task Page

--- client/src/pages/tasks/EditTask.jsx
+++ client/src/pages/tasks/EditTask.jsx
@@ -88,83 +88,94 @@ export default function EditTask() {
           </Link>
         </div>
       )}
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmitForm)}>
-        <div className="mt-10">
-          <fieldset className="border border-gray-300 rounded w-full">
-            <legend className="ml-5 text-lg text-(--lorem-color) mb-2">
-              Task Title
-            </legend>
-            <textarea
-              className="w-full h-8 outline-none px-5 text-xs border-none rounded resize-none"
-              {...register("title")}
-            />
-          </fieldset>
-          <p className="text-red-500 text-xs mt-1">{errors.title?.message}</p>
-        </div>
-        <div className="mt-10">
-          <fieldset className="border border-gray-300 rounded w-full">
-            <legend className="ml-5 text-lg text-(--lorem-color) mb-2">
-              Description
-            </legend>
-            <textarea
-              className="w-full h-40 outline-none px-5 text-xs border-none rounded resize-none"
-              {...register("description")}
-            />
-          </fieldset>
-          <p className="text-red-500 text-xs mt-1">
-            {errors.description?.message}
-          </p>
-        </div>
-        <div className="mt-10 relative">
-          <fieldset className="border border-gray-300 rounded w-full pb-2">
-            <legend className="ml-5 text-lg text-(--lorem-color) px-2">
-              Tags
-            </legend>
-            <div className="px-5 flex justify-between items-center w-full pt-1 pb-3">
-              <span className="text-gray-500 text-xs">Select Options</span>
-              <select
-                className={`text-xs border border-gray-500 outline-none p-1 rounded  ${
-                  tagStyles[selectedTag] || tagStyles.default
-                }`}
-                {...register("tags")}
-              >
-                <option value="" disabled>
-                  Select options
-                </option>
-                <option value="Urgent" className="text-red-500">
-                  Urgent
-                </option>
-                <option value="Important" className="text-(--important-green)">
-                  Important
-                </option>
-                <option value="Personal" className="text-blue-500">
-                  Personal
-                </option>
-              </select>
+      {!isPending && !isError && (
+        <>
+          <form className="flex flex-col" onSubmit={handleSubmit(onSubmitForm)}>
+            <div className="mt-10">
+              <fieldset className="border border-gray-300 rounded w-full">
+                <legend className="ml-5 text-lg text-(--lorem-color) mb-2">
+                  Task Title
+                </legend>
+                <textarea
+                  className="w-full h-8 outline-none px-5 text-xs border-none rounded resize-none"
+                  {...register("title")}
+                />
+              </fieldset>
+              <p className="text-red-500 text-xs mt-1">
+                {errors.title?.message}
+              </p>
             </div>
-          </fieldset>
-          <p className="text-red-500 text-xs mt-1">{errors.tags?.message}</p>
-        </div>
-        <button
-          className="bg-(--primary-color) text-white text-lg rounded w-full px-6 py-2 mt-10 flex justify-center items-center gap-2"
-          disabled={mutation.isPending}
-        >
-          {mutation.isPending ? (
-            <>
-              <Loader className="animate-spin" size={18} />
-              Saving...
-            </>
-          ) : (
-            "Done"
-          )}
-        </button>
-      </form>
-      <button
-        onClick={scrollUp}
-        className="underline text-(--primary-color) cursor-pointer my-2 text-center w-full"
-      >
-        Back To Top
-      </button>
+            <div className="mt-10">
+              <fieldset className="border border-gray-300 rounded w-full">
+                <legend className="ml-5 text-lg text-(--lorem-color) mb-2">
+                  Description
+                </legend>
+                <textarea
+                  className="w-full h-40 outline-none px-5 text-xs border-none rounded resize-none"
+                  {...register("description")}
+                />
+              </fieldset>
+              <p className="text-red-500 text-xs mt-1">
+                {errors.description?.message}
+              </p>
+            </div>
+            <div className="mt-10 relative">
+              <fieldset className="border border-gray-300 rounded w-full pb-2">
+                <legend className="ml-5 text-lg text-(--lorem-color) px-2">
+                  Tags
+                </legend>
+                <div className="px-5 flex justify-between items-center w-full pt-1 pb-3">
+                  <span className="text-gray-500 text-xs">Select Options</span>
+                  <select
+                    className={`text-xs border border-gray-500 outline-none p-1 rounded  ${
+                      tagStyles[selectedTag] || tagStyles.default
+                    }`}
+                    {...register("tags")}
+                  >
+                    <option value="" disabled>
+                      Select options
+                    </option>
+                    <option value="Urgent" className="text-red-500">
+                      Urgent
+                    </option>
+                    <option
+                      value="Important"
+                      className="text-(--important-green)"
+                    >
+                      Important
+                    </option>
+                    <option value="Personal" className="text-blue-500">
+                      Personal
+                    </option>
+                  </select>
+                </div>
+              </fieldset>
+              <p className="text-red-500 text-xs mt-1">
+                {errors.tags?.message}
+              </p>
+            </div>
+            <button
+              className="bg-(--primary-color) text-white text-lg rounded w-full px-6 py-2 mt-10 flex justify-center items-center gap-2"
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending ? (
+                <>
+                  <Loader className="animate-spin" size={18} />
+                  Saving...
+                </>
+              ) : (
+                "Done"
+              )}
+            </button>
+          </form>
+          <button
+            onClick={scrollUp}
+            className="underline text-(--primary-color) cursor-pointer my-2 text-center w-full"
+          >
+            Back To Top
+          </button>
+        </>
+      )}
     </div>
   );
 }
